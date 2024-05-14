29019f2c531d70b55a3fa7cb5ce27d1e4b35e19a
onumam21 <onumam56@gmail.com>
2024-05-14 15:38:06 +0700

    EX2-2.js

---
+++ EX2-1.js
@@ -0,0 +1,7 @@
+const isEven = function(number) {
+    return number % 2 === 0;
+  };
+  
+  // ทดสอบการเรียกใช้งานฟังก์ชัน
+  console.log(isEven(4)); // Output: true
+  console.log(isEven(7)); // Output: false
---
+++ Ex2-2.js
@@ -0,0 +1,5 @@
+const isEven = number => number % 2 === 0;
+
+// ทดสอบการเรียกใช้งานฟังก์ชัน
+console.log(isEven(4)); // Output: true
+console.log(isEven(7)); // Output: false
